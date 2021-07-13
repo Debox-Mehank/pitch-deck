50ff579623ddd97590b176711f445b3b54686231
Debox-Mehank <mehankjain1670@gmail.com>
2021-07-13 20:54:53 +0530

    changes

--- index.js
+++ index.js
@@ -9,54 +9,26 @@ Reveal.addEventListener("slidechanged", function (evt) {
     document.getElementById("custom-down").classList.add("hidden");
     document.getElementById("custom-down").classList.remove("visible");
   }
-  // console.log(Reveal.availableRoutes());
-  // console.log(evt.currentSlide);
 });
 
-// const mQuery = window.matchMedia(
-//   "screen and (min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 1)"
-// );
+const mQuery = window.matchMedia(
+  "screen and (min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 1)"
+);
 
-// function handleMobilePhoneResize(e) {
-//   // Check if the media query is true
-//   if (e.matches) {
-//     // var myobj = document.getElementById("test-js");
-//     // myobj.setAttribute(
-//     //   "data-background-image",
-//     //   "images/Debox_Deck_Presentation-08.jpg"
-//     // );
-//     // myobj.remove();
-//     var newElem = document.createElement("section");
-//     newElem.id = "test-js-2";
-//     newElem.setAttribute(
-//       "data-background-image",
-//       "images/Debox_Deck_Presentation-09.jpg"
-//     );
-//     newElem.setAttribute("data-background-size", "cover");
-//     newElem.setAttribute("data-background-position", "center");
-//     var s7 = document.getElementById("s7");
-//     s7.prepend(newElem);
-//   } else {
-//     // var myobj = document.getElementById("test-js");
-//     // myobj.setAttribute(
-//     //   "data-background-image",
-//     //   "images/Debox_Deck_Presentation-07.jpg"
-//     // );
-//     // myobj.remove();
-//     var newElem = document.createElement("section");
-//     newElem.id = "test-js-3";
-//     newElem.setAttribute(
-//       "data-background-image",
-//       "images/Debox_Deck_Presentation-08.jpg"
-//     );
-//     newElem.setAttribute("data-background-size", "cover");
-//     newElem.setAttribute("data-background-position", "center");
-//     var s7 = document.getElementById("s7");
-//     s7.prepend(newElem);
-//   }
-// }
+function handleMobilePhoneResize(e) {
+  // const s7 = document.getElementById("sm-main");
+  // const s7M = document.getElementById("sm-main-mobile");
+  // // Check if the media query is true
+  // if (e.matches) {
+  //   s7M.removeAttribute("data-visibility");
+  //   s7.setAttribute("data-visibility", "hidden");
+  // } else {
+  //   s7.removeAttribute("data-visibility");
+  //   s7M.setAttribute("data-visibility", "hidden");
+  // }
+}
 
-// // Set up event listener
-// mQuery.addListener(handleMobilePhoneResize);
+// Set up event listener
+mQuery.addListener(handleMobilePhoneResize);
 
-// handleMobilePhoneResize(mQuery);
+handleMobilePhoneResize(mQuery);
